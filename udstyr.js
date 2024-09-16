4a9937a90aa9e98a1bded77668c69662419f92d8
Viktor <viho0002@stud.kea.dk>
2024-09-16 13:19:34 +0200

    javascript opdatering

--- udstyr.js
+++ udstyr.js
@@ -7,7 +7,7 @@ const query = urlParams.get("Asset-ID");
 console.log(query);
 
 const id = "C00900";
-const url = `https://mbfwwvemximslgoumeom.supabase.co/rest/v1/TSL?Asset-ID=eq.${id}`;
+const url = `https://mbfwwvemximslgoumeom.supabase.co/rest/v1/TSL?AssetID=eq.${id}`;
 
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZnd3dmVteGltc2xnb3VtZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjY4NTQsImV4cCI6MjA0MTQ0Mjg1NH0.UBXPhxZr5Vah5GRBQ_IW5gqIWf0HjOMdbIsHklKN3go";
 
