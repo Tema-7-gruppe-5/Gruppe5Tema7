6f3c26915ccc0637e3b8d452dcd053314fab1c8a
Viktor <viho0002@stud.kea.dk>
2024-09-18 01:09:33 +0200

    javascript motor

--- udstyr.js
+++ udstyr.js
@@ -1,28 +1,15 @@
-//https://mbfwwvemximslgoumeom.supabase.co
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZnd3dmVteGltc2xnb3VtZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjY4NTQsImV4cCI6MjA0MTQ0Mjg1NH0.UBXPhxZr5Vah5GRBQ_IW5gqIWf0HjOMdbIsHklKN3go
-window.addEventListener("DOMContentLoaded", init);
-
 const urlParams = new URLSearchParams(window.location.search);
-const query = urlParams.get("Asset-ID");
-console.log(query);
+const id = urlParams.get("AssetID");
+console.log(id);
 
-const id = "C00900";
-const url = `https://mbfwwvemximslgoumeom.supabase.co/rest/v1/TSL?AssetID=eq.${id}`;
-
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZnd3dmVteGltc2xnb3VtZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjY4NTQsImV4cCI6MjA0MTQ0Mjg1NH0.UBXPhxZr5Vah5GRBQ_IW5gqIWf0HjOMdbIsHklKN3go";
-
-const options = {
+fetch(`https://mbfwwvemximslgoumeom.supabase.co/rest/v1/TSL?AssetID=eq.${id}`, {
   headers: {
-    apikey: key,
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZnd3dmVteGltc2xnb3VtZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjY4NTQsImV4cCI6MjA0MTQ0Mjg1NH0.UBXPhxZr5Vah5GRBQ_IW5gqIWf0HjOMdbIsHklKN3go",
   },
-};
+})
+  .then((res) => res.json())
+  .then(showList);
 
-function init() {
-  fetch(url, options)
-    .then((res) => res.json())
-    .then(showData);
-}
-
-function showData(items) {
-  console.log(items[0]);
+function showList(data) {
+  console.log(data[0]);
 }
--- underkategorier.js
+++ underkategorier.js
@@ -1,13 +1,33 @@
+window.addEventListener("DOMContentLoaded", init);
+
 const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get("Type");
-console.log(query);
 
-const id = "C00900";
+function init() {
+  productList = document.querySelector("#product_list");
+  console.log("product_list", productList);
+  productTemplate = document.querySelector("template").content;
+  console.log("productTemplate", productTemplate);
 
-fetch(`https://mbfwwvemximslgoumeom.supabase.co/rest/v1/TSL?Type=eq.${query}`, {
-  headers: {
-    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZnd3dmVteGltc2xnb3VtZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjY4NTQsImV4cCI6MjA0MTQ0Mjg1NH0.UBXPhxZr5Vah5GRBQ_IW5gqIWf0HjOMdbIsHklKN3go",
-  },
-})
-  .then((res) => res.json())
-  .then(console.log);
+  fetch(`https://mbfwwvemximslgoumeom.supabase.co/rest/v1/TSL?Type=eq.${query}`, {
+    headers: {
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZnd3dmVteGltc2xnb3VtZW9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU4NjY4NTQsImV4cCI6MjA0MTQ0Mjg1NH0.UBXPhxZr5Vah5GRBQ_IW5gqIWf0HjOMdbIsHklKN3go",
+    },
+  })
+    .then((res) => res.json())
+    .then(showList2);
+}
+
+function showList2(json) {
+  console.log("json", json);
+  json.forEach(showProduct);
+}
+
+function showProduct(product) {
+  const clone = productTemplate.cloneNode(true);
+  clone.querySelector("h3").textContent = `${product.Produktnavn_model}`;
+  clone.querySelector("h2").textContent = `${product.Taksonomi1}`;
+  console.log("hejsa", product.Taksonomi1);
+  clone.querySelector("a").href = `udstyr.html?AssetID=${product.AssetID}`;
+  productList.appendChild(clone);
+}
