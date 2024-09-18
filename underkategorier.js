ae826050b237825ddf462da04b80395b9a0d7caf
bjørn <bjornhoj1@gmail.com>
2024-09-18 13:35:44 +0200

    Underkategori, M. header

--- underkategorier.js
+++ underkategorier.js
@@ -31,3 +31,9 @@ function showProduct(product) {
   clone.querySelector("img").src = "img/" + product.imgs;
   productList.appendChild(clone);
 }
+
+document.getElementById('burger-menu').addEventListener('click', function() {
+  const mainMenu = document.querySelector('.main-menu');
+  
+  mainMenu.classList.toggle('active'); // Toggler visningen af menuen på små skærme
+});
